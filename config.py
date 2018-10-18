673e607bfcbf5c60857d6e91a249ae764cba00cf
dataplayer12 <jaiyamsharma@gmail.com>
2018-10-18 17:01:19 +0900

    added code until now

---
+++ config.py
@@ -0,0 +1,10 @@
+h264_folder='h264_videos/'
+mp4_folder='mp4_videos/'
+log_dir='logs/'
+logfile='files_not_sent.txt'
+countfile='countfile.txt'
+
+motion_threshold=25
+initial_sleep=3
+send_time_threshold=-1 #no threshold
+location='Asia/Kolkata'
