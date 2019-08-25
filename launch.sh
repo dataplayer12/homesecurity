bbd34ad3d0081e78cb5a38089be9d8bda61aa90f
dataplayer12 <jaiyamsharma@gmail.com>
2019-08-25 21:40:51 +0900

    improved launch script and logs

--- launch.sh
+++ launch.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-device="jetsonano" #'raspi3','jetsonano','streampi','jetsontx2'
+BASEDIR=$(pwd)
 
-if [ "$device" == "streampi" ]; then
+DEVICE="jetsonano"
+#OPTIONS: 'raspi3','jetsonano','streampi','jetsontx2'
+#In principle, the device streampi can be anything,
+#but, a pi zero is most commonly used.
+
+if [ ! -d "$BASEDIR/h264_videos/" ]
+then
+    mkdir h264_videos
+    echo 'Setting up h264 directory'
+fi
+
+if [ ! -d "$BASEDIR/mp4_videos/" ]
+then
+    mkdir mp4_videos
+    echo 'Setting up MP4 directory'
+fi
+
+
+if [ "$DEVICE" == "streampi" ]; then
+
 	cd common/
 	python3 stream_video.py
-elif [ "$device" == "raspi3" ]; then
-	#cd raspi3/
-	#logname="logs/camlog$(ls -l logs|wc -l).txt"
-	#touch $logname
-	#echo $logname
+
+elif [ "$DEVICE" == "raspi3" ]; then
+
 	nohup python3 -u raspi3/securitycam.py &> logs/camlog$(ls -l ./logs|wc -l).txt &
-	#nohup python3 securitycam.py &
-elif [ "$device" == "jetsonano" ]; then
-	#cd jetsonano/
+
+elif [ "$DEVICE" == "jetsonano" ]; then
+
 	python3 jetsonano/securitycamnano.py
-elif [ "$device" == "jetsontx2" ]; then
+
+elif [ "$DEVICE" == "jetsontx2" ]; then
 	cd jetsontx2/
 	python3 stream_surveillance.py
 fi
